ba62db97e95e87b011d9cc68aade162ef7c8d750
Florian Klink <flokli@flokli.de>
2019-11-27 19:33:37 +0100

    result: move code into separate result.go

---
+++ submitqueue/result.go
@@ -0,0 +1,54 @@
+package submitqueue
+
+import (
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+// Problem: no inspection during the run
+// Problem: record the state
+
+// Result contains all data necessary to inspect a previous run
+// This includes the Series from that run, and all Log Entries collected.
+// It also implements the interface required for logrus.Hook.
+type Result struct {
+	LogEntries []*logrus.Entry
+	Series     []Serie
+	Error      error
+	startTime  time.Time
+}
+
+// MakeResult produces a new Result struct,
+// and initializes startTime with the current time.
+func MakeResult() *Result {
+	return &Result{
+		startTime: time.Now(),
+	}
+}
+
+// StartTime returns the startTime
+func (r Result) StartTime() time.Time {
+	return r.startTime
+}
+
+// EndTime returns the time of the latest log entry
+func (r Result) EndTime() time.Time {
+	if len(r.LogEntries) == 0 {
+		return r.startTime
+	}
+	return r.LogEntries[len(r.LogEntries)-1].Time
+}
+
+// Fire is called by logrus on each log event,
+// we collect all log entries in the struct variable
+func (r *Result) Fire(entry *logrus.Entry) error {
+	r.LogEntries = append(r.LogEntries, entry)
+	return nil
+}
+
+// Levels is called by logrus to determine whether to Fire the handler.
+// As we want to collect all log entries, we return logrus.AllLevels
+func (r *Result) Levels() []logrus.Level {
+	return logrus.AllLevels
+}
--- submitqueue/submitqueue.go
+++ submitqueue/submitqueue.go
@@ -2,7 +2,6 @@ package submitqueue
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/tweag/gerrit-queue/gerrit"
 
@@ -154,53 +153,6 @@ func (s *SubmitQueue) DoRebase(log *logrus.Logger) error {
 	}
 
 	return nil
-}
-
-// Problem: no inspection during the run
-// Problem: record the state
-
-// Result all data necessary to inspect a previous run
-// This includes the Series from that run, and all Log Entries collected.
-// It also implements the interface required for logrus.Hook.
-type Result struct {
-	LogEntries []*logrus.Entry
-	Series     []Serie
-	Error      error
-	startTime  time.Time
-}
-
-// MakeResult produces a new Result struct,
-// and initializes startTime with the current time.
-func MakeResult() *Result {
-	return &Result{
-		startTime: time.Now(),
-	}
-}
-
-// StartTime returns the startTime
-func (r Result) StartTime() time.Time {
-	return r.startTime
-}
-
-// EndTime returns the time of the latest log entry
-func (r Result) EndTime() time.Time {
-	if len(r.LogEntries) == 0 {
-		return r.startTime
-	}
-	return r.LogEntries[len(r.LogEntries)-1].Time
-}
-
-// Fire is called by logrus on each log event,
-// we collect all log entries in the struct variable
-func (r *Result) Fire(entry *logrus.Entry) error {
-	r.LogEntries = append(r.LogEntries, entry)
-	return nil
-}
-
-// Levels is called by logrus to determine whether to Fire the handler.
-// As we want to collect all log entries, we return logrus.AllLevels
-func (r *Result) Levels() []logrus.Level {
-	return logrus.AllLevels
 }
 
 // Run starts the submit and rebase logic.
